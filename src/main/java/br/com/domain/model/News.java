c2b5cd0953cb45bd54dbef28c7ff6e8e65620ffc
maxjdev <maxjramosdev@gmail.com>
2024-03-04 19:08:12 -0300

    add: domain layer, entity modeling & database configs

---
+++ src/main/java/br/com/domain/model/News.java
@@ -0,0 +1,6 @@
+package br.com.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {}
---
+++ src/main/java/br/com/domain/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package br.com.domain.repository;
+
+import br.com.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> { }
